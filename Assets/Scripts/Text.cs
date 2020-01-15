89b424dc24f356badeb8eb04cde278bcdaba53a3
Unknown <kawaxbart@gmail.com>
2020-01-15 15:12:54 +0100

    Add showing text

---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    public void ShowForSeconds(int seconds)
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(HideAfterSeconds(seconds));
+    }
+
+    IEnumerator HideAfterSeconds(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        gameObject.SetActive(false);
+    }
+}
