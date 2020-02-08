e11fdd2d76f8dc4325df2121bf12de760d2d8c90
Unknown <kawaxbart@gmail.com>
2020-02-08 13:27:22 +0100

    Add disabling button for 3 seconds

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.UI.Button))]
+public class Button : MonoBehaviour
+{
+    public void DisableForSeconds(int seconds)
+    {
+        GetComponent<UnityEngine.UI.Button>().interactable = false;
+        StartCoroutine(EnableAfterSeconds(seconds));
+    }
+
+    IEnumerator EnableAfterSeconds(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        GetComponent<UnityEngine.UI.Button>().interactable = true;
+    }
+}
